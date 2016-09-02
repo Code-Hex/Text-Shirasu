f94f4578cae0439b5fbb7d2fcbbb98e6b7f0cdf7
K <x00.x7f@gmail.com>
2016-09-02 17:57:47 +0900

    initial init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Text::MeCab::Easy - It's new $module
+
+# SYNOPSIS
+
+    use Text::MeCab::Easy;
+
+# DESCRIPTION
+
+Text::MeCab::Easy is ...
+
+# LICENSE
+
+Copyright (C) K.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+K <x00.x7f@gmail.com>
---
+++ lib/Text/MeCab/Easy.pm
@@ -0,0 +1,39 @@
+package Text::MeCab::Easy;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Text::MeCab::Easy - It's new $module
+
+=head1 SYNOPSIS
+
+    use Text::MeCab::Easy;
+
+=head1 DESCRIPTION
+
+Text::MeCab::Easy is ...
+
+=head1 LICENSE
+
+Copyright (C) K.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+K E<lt>x00.x7f@gmail.comE<gt>
+
+=cut
+
