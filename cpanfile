112313cea060fae0be181c0d17c057a84131eced
K <x00.x7f@gmail.com>
2016-09-02 17:58:11 +0900

    Created Easy.pm

--- cpanfile
+++ cpanfile
@@ -1,3 +1,4 @@
+requires 'Text::MeCab';
 requires 'perl', '5.008001';
 
 on 'test' => sub {
--- lib/Text/MeCab/Easy.pm
+++ lib/Text/MeCab/Easy.pm
@@ -1,10 +1,76 @@
 package Text::MeCab::Easy;
-use 5.008001;
+
 use strict;
 use warnings;
+use v5.10;
+
+use Encode;
+use Text::MeCab;
 
 our $VERSION = "0.01";
 
+sub mecab    { $_[0]->{mecab}    }
+sub surfaces { $_[0]->{surfaces} }
+sub features { $_[0]->{features} }
+
+sub new {
+    my $class = shift;
+    my %args = ref $_[0] eq 'HASH' ? %{$_[0]} : @_;
+    return bless {
+        mecab => Text::MeCab->new(%args),
+        surfaces => [],
+        features => []
+    }, $class;
+}
+
+sub parse {
+    my ($self, $sentence) = @_;
+
+    my $mt = $self->{mecab};
+
+    # initialize
+    $self->{surfaces} = [];
+    $self->{features} = [];
+
+    for (my $node = $mt->parse($sentence); $node && $node->surface; $node = $node->next) {
+        push @{$self->{surfaces}}, $node->surface;
+        push @{$self->{features}}, [split /,/, $node->feature];
+    }
+
+    return $self;
+}
+
+sub filter {
+    my $self = shift;
+
+    # filter by part of speech
+    my $judge = @_ > 1 ? join '|', map { encode_utf8($_) } @_ : encode_utf8(shift);
+
+    my $cnt = @{$self->{surfaces}};
+    my $surfaces = [];
+    my $features = [];
+
+    for my $i (0 .. $cnt) {
+        if ($self->{features}->[$i]->[0] =~ /$judge/) {
+            push @$surfaces, $self->{surfaces}->[$i];
+            push @$features, $self->{features}->[$i];
+        }
+    }
+
+    return ($surfaces, $features);
+}
+
+sub dumper {
+    my $self = shift;
+
+    my $cnt = @{$self->{surfaces}};
+
+    print "\n";
+    for my $i (0 .. $cnt) {
+        printf "%s:\t%s\n", $self->{surfaces}->[$i], join ',', @{$self->{features}};
+    }
+    print "\n";
+}
 
 
 1;
