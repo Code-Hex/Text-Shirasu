666dde516e16e7000703e6a41098fb45912da5b6
K <x00.x7f@gmail.com>
2016-09-15 17:48:41 +0900

    Added README

--- README.md
+++ README.md
@@ -1,28 +1,33 @@
 # NAME
 
-Text::Shirasu - It's new $module
+Text::Shirasu - Text::MeCab wrapper
 
 # SYNOPSIS
 
     use utf8;
     use Text::Shirasu;
-    my $mt = Text::Shirasu->new;
-    my $parse = $mt->parse("昨日の晩御飯は「鮭のふりかけ」と「味噌汁」だけでした。");
+    my $ts = Text::Shirasu->new;
+    my $parse = $ts->parse("昨日の晩御飯は「鮭のふりかけ」と「味噌汁」だけでした。");
 
+    use Data::Dumper;
     my $search = $parse->search(type => [qw/名詞 助動詞/], 記号 => [qw/括弧開 括弧閉/]);
     print Dumper $search->result;
 
+    my $tr = $parse->parse('。' => '.');
+    print Dumper $tr->result;
+
 # DESCRIPTION
 
-Text::Shirasu is ...
+Text::Shirasu wrapped [Text::MeCab](https://metacpan.org/pod/Text::MeCab).  
+This module has functions filter, replacement, etc...
 
 # LICENSE
 
-Copyright (C) K.
+Copyright (C) Kei Kamikawa(Code-Hex).
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 # AUTHOR
 
-K <x00.x7f@gmail.com>
+Kei Kamikawa <x00.x7f@gmail.com>
--- lib/Text/Shirasu.pm
+++ lib/Text/Shirasu.pm
@@ -137,31 +137,36 @@ __END__
 
 =head1 NAME
 
-Text::Shirasu - It's new $module
+Text::Shirasu - Text::MeCab wrapper
 
 =head1 SYNOPSIS
 
     use utf8;
     use Text::Shirasu;
-    my $mt = Text::Shirasu->new;
-    my $parse = $mt->parse("昨日の晩御飯は「鮭のふりかけ」と「味噌汁」だけでした。");
+    my $ts = Text::Shirasu->new;
+    my $parse = $ts->parse("昨日の晩御飯は「鮭のふりかけ」と「味噌汁」だけでした。");
 
+    use Data::Dumper;
     my $search = $parse->search(type => [qw/名詞 助動詞/], 記号 => [qw/括弧開 括弧閉/]);
     print Dumper $search->result;
 
+    my $tr = $parse->parse('。' => '.');
+    print Dumper $tr->result;
+
 =head1 DESCRIPTION
 
-Text::Shirasu is ...
+Text::Shirasu wrapped L<Text::MeCab>.  
+This module has functions filter, replacement, etc...
 
 =head1 LICENSE
 
-Copyright (C) K.
+Copyright (C) Kei Kamikawa(Code-Hex).
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =head1 AUTHOR
 
-K E<lt>x00.x7f@gmail.comE<gt>
+Kei Kamikawa E<lt>x00.x7f@gmail.comE<gt>
 
 =cut
