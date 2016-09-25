d22111bd176fe3d088c49c92d986c5bf791e5742
K <x00.x7f@gmail.com>
2016-09-26 00:46:16 +0900

    pod の修正

--- lib/Text/Shirasu.pm
+++ lib/Text/Shirasu.pm
@@ -52,16 +52,13 @@ sub new {
     } => $class;
 }
 
-
-=over 4
-=item B<parse>
+=head2 parse
 
     $ts->parse("このおにぎりは「母」が握ってくれたものです。");
 
 Text::MeCab の parse メソッドをラッピングしています。
 parse メソッドは実行結果をオブジェクト内に保存し、オブジェクトを返します。
 用意されている他のメソッドを使用すると結果を上書きして再びオブジェクト内に保存されることに注意してください。
-=back
 =cut
 
 sub parse {
@@ -94,14 +91,12 @@ sub parse {
     return $self;
 }
 
-=over 4
-=item B<tr>
+=head2 tr
 
     $ts->tr('，！？' => ',!?');
 
 parse メソッド実行後にオブジェクト内に保存されている surface の文字列を置換します。
 Perl の tr と同じように使います。実行結果はオブジェクト内に保存されます。
-=back
 =cut
 
 sub tr {
@@ -128,8 +123,7 @@ sub tr {
     return $self;
 }
 
-=over 4
-=item B<search>
+=head2 search
     
     $ts->search(type => [qw/名詞/]);
     $ts->search(type => [qw/名詞 記号/], 記号 => [qw/括弧開 括弧閉/]);
@@ -137,7 +131,7 @@ sub tr {
 parse メソッド実行後にオブジェクト内に保存されている surface の文字列を feature の情報を利用して条件をもとに絞り込みます。
 type をキーに欲しい品詞の情報を渡します。さらにその品詞の中から細かく絞り込みたい時は、その品詞名をキーにして、細かい情報を渡します。
 実行結果はオブジェクト内に保存されます。
-=back
+
 =cut
 
 sub search {
