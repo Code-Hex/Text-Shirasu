6ffcf1878fcdd05772dd9c6ce65c14d0a468204c
K <x00.x7f@gmail.com>
2016-09-26 00:40:06 +0900

    Added pod

--- README.md
+++ README.md
@@ -21,6 +21,27 @@ Text::Shirasu - Text::MeCab wrapper
 Text::Shirasu wrapped [Text::MeCab](https://metacpan.org/pod/Text::MeCab).  
 This module has functions filter, replacement, etc...
 
+>     $ts->parse("このおにぎりは「母」が握ってくれたものです。");
+>
+> Text::MeCab の parse メソッドをラッピングしています。
+> parse メソッドは実行結果をオブジェクト内に保存し、オブジェクトを返します。
+> 用意されている他のメソッドを使用すると結果を上書きして再びオブジェクト内に保存されることに注意してください。
+> &#x3d;back
+>
+> >     $ts->tr('，！？' => ',!?');
+> >
+> > parse メソッド実行後にオブジェクト内に保存されている surface の文字列を置換します。
+> > Perl の tr と同じように使います。実行結果はオブジェクト内に保存されます。
+> > &#x3d;back
+> >
+> > >     $ts->search(type => [qw/名詞/]);
+> > >     $ts->search(type => [qw/名詞 記号/], 記号 => [qw/括弧開 括弧閉/]);
+> > >
+> > > parse メソッド実行後にオブジェクト内に保存されている surface の文字列を feature の情報を利用して条件をもとに絞り込みます。
+> > > type をキーに欲しい品詞の情報を渡します。さらにその品詞の中から細かく絞り込みたい時は、その品詞名をキーにして、細かい情報を渡します。
+> > > 実行結果はオブジェクト内に保存されます。
+> > > &#x3d;back
+
 # LICENSE
 
 Copyright (C) Kei Kamikawa(Code-Hex).
@@ -31,3 +52,27 @@ it under the same terms as Perl itself.
 # AUTHOR
 
 Kei Kamikawa <x00.x7f@gmail.com>
+
+# POD ERRORS
+
+Hey! **The above document had some coding errors, which are explained below:**
+
+- Around line 56:
+
+    &#x3d;over should be: '=over' or '=over positive\_number'
+
+- Around line 97:
+
+    &#x3d;over should be: '=over' or '=over positive\_number'
+
+- Around line 131:
+
+    &#x3d;over should be: '=over' or '=over positive\_number'
+
+- Around line 197:
+
+    You forgot a '=back' before '=head1'
+
+    You forgot a '=back' before '=head1'
+
+    You forgot a '=back' before '=head1'
--- lib/Text/Shirasu.pm
+++ lib/Text/Shirasu.pm
@@ -15,6 +15,34 @@ use constant DEBUG => 1;
 sub mecab  { $_[0]->{mecab}  }
 sub result { $_[0]->{result} }
 
+
+=encoding utf-8
+
+=head1 NAME
+
+Text::Shirasu - Text::MeCab wrapper
+
+=head1 SYNOPSIS
+
+    use utf8;
+    use feature ':5.10';
+    use Text::Shirasu;
+    my $ts = Text::Shirasu->new; # this parameter same as Text::MeCab
+    my $parse = $ts->parse("昨日の晩御飯は「鮭のふりかけ」と「味噌汁」だけでした。");
+
+    my $tr = $parse->tr('。' => '.');
+    say $tr->join_surface;
+    
+    my $search = $parse->search(type => [qw/名詞 助動詞/], 記号 => [qw/括弧開 括弧閉/]);
+    say $search->join_surface;
+
+=head1 DESCRIPTION
+
+Text::Shirasu wrapped L<Text::MeCab>.  
+This module has functions filter, replacement, etc...
+
+=cut
+
 sub new {
     my $class = shift;
     my %args = ref $_[0] eq 'HASH' ? %{$_[0]} : @_;
@@ -23,6 +51,18 @@ sub new {
         result => +[]
     } => $class;
 }
+
+
+=over 4
+=item B<parse>
+
+    $ts->parse("このおにぎりは「母」が握ってくれたものです。");
+
+Text::MeCab の parse メソッドをラッピングしています。
+parse メソッドは実行結果をオブジェクト内に保存し、オブジェクトを返します。
+用意されている他のメソッドを使用すると結果を上書きして再びオブジェクト内に保存されることに注意してください。
+=back
+=cut
 
 sub parse {
     my $self = shift;
@@ -54,6 +94,16 @@ sub parse {
     return $self;
 }
 
+=over 4
+=item B<tr>
+
+    $ts->tr('，！？' => ',!?');
+
+parse メソッド実行後にオブジェクト内に保存されている surface の文字列を置換します。
+Perl の tr と同じように使います。実行結果はオブジェクト内に保存されます。
+=back
+=cut
+
 sub tr {
     my $self = shift;
     my %params = ref $_[0] eq 'HASH' ? 
@@ -77,6 +127,18 @@ sub tr {
 
     return $self;
 }
+
+=over 4
+=item B<search>
+    
+    $ts->search(type => [qw/名詞/]);
+    $ts->search(type => [qw/名詞 記号/], 記号 => [qw/括弧開 括弧閉/]);
+
+parse メソッド実行後にオブジェクト内に保存されている surface の文字列を feature の情報を利用して条件をもとに絞り込みます。
+type をキーに欲しい品詞の情報を渡します。さらにその品詞の中から細かく絞り込みたい時は、その品詞名をキーにして、細かい情報を渡します。
+実行結果はオブジェクト内に保存されます。
+=back
+=cut
 
 sub search {
     my $self = shift;
@@ -131,32 +193,6 @@ sub _sub_query {
 
 
 1;
-__END__
-
-=encoding utf-8
-
-=head1 NAME
-
-Text::Shirasu - Text::MeCab wrapper
-
-=head1 SYNOPSIS
-
-    use utf8;
-    use feature ':5.10';
-    use Text::Shirasu;
-    my $ts = Text::Shirasu->new; # this parameter same as Text::MeCab
-    my $parse = $ts->parse("昨日の晩御飯は「鮭のふりかけ」と「味噌汁」だけでした。");
-
-    my $tr = $parse->tr('。' => '.');
-    say $tr->join_surface;
-    
-    my $search = $parse->search(type => [qw/名詞 助動詞/], 記号 => [qw/括弧開 括弧閉/]);
-    say $search->join_surface;
-
-=head1 DESCRIPTION
-
-Text::Shirasu wrapped L<Text::MeCab>.  
-This module has functions filter, replacement, etc...
 
 =head1 LICENSE
 
